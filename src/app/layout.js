e8e5600247199beee4e1d5865430107bcb9b2083
boniface193 <bonifacedennis99@gmail.com>
2025-09-17 17:08:42 +0100

    optimized website added search console file

--- src/app/layout.js
+++ src/app/layout.js
@@ -53,6 +53,7 @@ export default function RootLayout({ children }) {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="icons/favicon-144x144.png" />
         <meta name="msapplication-config" content="icons/browserconfig.xml" />
+        <meta name="google-site-verification" content="Ix3-VxBvXFmoDRhM6FSNYdUTME-ulJlKR-lLLcg0u10" />
       </head>
       <body>
         <Navbar />
