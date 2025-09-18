631bdb04dafba430665f83db69c9f1f1a57837dc
boniface193 <bonifacedennis99@gmail.com>
2025-09-18 19:53:08 +0100

    image optimized

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    domains: ['https://viveeaura.com'],
+    formats: ['image/webp', 'image/avif'], // Modern image formats
     remotePatterns: [new URL('https://viveeaura.org/wp-content/uploads/***')],
   },
 };
